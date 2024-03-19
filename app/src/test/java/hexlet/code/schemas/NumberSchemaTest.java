7e82210193c8252e5a676567f1a5769482dcdd40
Roman <voronovyyyy@yandex.ru>
2024-03-19 22:51:59 +0300

    add final in test

--- app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NumberSchemaTest {
+public final class NumberSchemaTest {
     private BaseSchema numberSchema;
 
     @BeforeEach
--- app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class StringSchemaTest {
+public final class StringSchemaTest {
     private BaseSchema stringSchema;
 
     @BeforeEach
