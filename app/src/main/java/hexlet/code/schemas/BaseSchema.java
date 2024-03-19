69b4131dfcfc3e57af733522907582dea6765ddb
Roman <voronovyyyy@yandex.ru>
2024-03-19 22:30:10 +0300

    refactor for generic

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,8 +4,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema {
-    protected Map<String, Predicate<Object>> checks = new LinkedHashMap<>();
+public abstract class BaseSchema<T> {
+    protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
     protected boolean isRequired;
 
     public abstract BaseSchema required();
@@ -14,12 +14,12 @@ public abstract class BaseSchema {
         isRequired = true;
     }
 
-    public final boolean isValid(Object validatingObject) {
+    public final boolean isValid(T validatingObject) {
         if (validatingObject == null) {
             return !isRequired;
         }
 
-        for (Map.Entry<String, Predicate<Object>> check : checks.entrySet()) {
+        for (Map.Entry<String, Predicate<T>> check : checks.entrySet()) {
             if (!check.getValue().test(validatingObject)) {
                 return false;
             }
@@ -27,7 +27,7 @@ public abstract class BaseSchema {
         return true;
     }
 
-    protected final void addCheck(String checkToAddName, Predicate<Object> checkToAdd) {
+    protected final void addCheck(String checkToAddName, Predicate<T> checkToAdd) {
         checks.put(checkToAddName, checkToAdd);
     }
 }
